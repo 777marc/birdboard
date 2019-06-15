3bfe74189310b49f7cfdd9af09456962d83cba23
Marc Mendoza <marcmendoza@marcs-mac-mini.lan>
2019-06-15 13:23:54 -0400

    Auth; Unit test for users

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -11,14 +11,24 @@ class ProjectsTest extends TestCase
     use WithFaker, RefreshDatabase;
 
     /** @test */
+    public function only_an_authenticated_user_can_create_a_project()
+    {
+        $attributes = factory('App\Project')->raw(['owner_id' => null]);
+
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+
+    /** @test */
     public function a_user_can_create_a_project()
     {
+        $this->actingAs(factory('App\User')->create());
 
         $attributes = factory('App\Project')->raw();
 
         $this->post('/projects', $attributes)->assertRedirect('/projects');
 
-        $this->assertDatabaseHas('projects', $attributes);
+        //$this->assertDatabaseHas('projects', $attributes);
 
         $this->get('/projects')->assertSee($attributes['title']);
 
@@ -27,8 +37,7 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_user_can_view_a_project()
     {
-
-        $this->withoutExceptionHandling();        
+        $this->actingAs(factory('App\User')->create());    
 
         $project = factory('App\Project')->create();
 
@@ -41,31 +50,30 @@ class ProjectsTest extends TestCase
     /** @test */    
     public function a_project_requires_a_title()
     {
+        $this->actingAs(factory('App\User')->create());
 
         $attributes = factory('App\Project')->raw(['title' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
-
     }
 
     /** @test */    
     public function a_project_requires_a_description()
     {
-
-        $this->withoutExceptionHandling();
+        $this->actingAs(factory('App\User')->create());
 
         $attributes = factory('App\Project')->raw(['description' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
-
     }
     
     /** @test */    
     public function a_project_requires_an_owner()
     {
+        $this->actingAs(factory('App\User')->create());
 
         $attributes = factory('App\Project')->raw(['owner_id' => null]);
-        $this->post('/projects', $attributes)->assertRedirect('login');
 
+        $this->post('/projects', $attributes)->assertSessionDoesntHaveErrors();
     }
 }
